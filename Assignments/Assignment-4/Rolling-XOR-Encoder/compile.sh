6f94e2a42e8caef716f147e0f0696bffcbc15cf9
jstigerwalt <johnstigerwalt@live.com>
2017-12-22 14:57:54 -0500

    Create compile.sh

---
+++ Assignments/Assignment-4/Rolling-XOR-Encoder/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '[+] Assembling with Nasm ... '
+nasm -f elf32 -o $1.o $1.nasm
+
+echo '[+] Linking ...'
+ld -o $1 $1.o
+
+echo '[+] Deleting Files...'
+rm $1.o
+
+echo '[+] Displaying Shellcode...'
+echo '------------------'
+objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^//'|sed 's/$//g'
+echo '------------------'
+echo '[+] Done!'
